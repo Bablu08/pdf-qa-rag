8260a0892ad29099bcdaa041b97ec8ad2e97a3c8
Bablu08 <rajkishorebehera358@gmail.com>
2026-07-30 17:40:12 +0530

    Refactor extract_text function and add chunk_text function for text processing

--- pdf_processor.py
+++ pdf_processor.py
@@ -9,3 +9,16 @@ def extract_text(uploded_file):
         text += page.extract_text() + "\n\n"
 
     return text
+
+def chunk_text(text, chunk_size, overlap):
+
+    chunks = []
+    start = 0
+
+    while start < len(text):
+        chunk = text[start : start + chunk_size]
+        chunks.append(chunk)
+
+        start += chunk_size - overlap
+
+    return chunks
