619bf7f95c1a05f8a7c93415909bad57ed06a1e4
Bablu08 <rajkishorebehera358@gmail.com>
2026-07-30 19:17:02 +0530

    Add client initialization and knowledge base collection setup in vector_store.py

--- vector_store.py
+++ vector_store.py
@@ -7,3 +7,6 @@ from sentence_transformers import SentenceTransformer, util
 @st.cache_resource
 def load_model():
     return SentenceTransformer('all-MiniLM-L6-v2')
+
+client = chromadb.Client()
+collection = client.get_or_create_collection(name="knowledge_base")
